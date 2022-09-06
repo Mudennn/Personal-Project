51942964d0a61f35bc50dbab81dbff2df461e899
Muden <mudennn@yahoo.com>
2022-09-06 09:52:40 +0800

    color change

--- NFT-Marketplace/src/components/Footer.js
+++ NFT-Marketplace/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <h3 className='dark:bg-gray-800 dark:text-gray-100 text-center py-6 mt-10 font-light'> Muden &copy; 2022</h3>
+        <h3 className='bg-gray-800 text-gray-100 text-center py-6 mt-10 font-light'> Muden &copy; 2022</h3>
     </div>
   )
 }
